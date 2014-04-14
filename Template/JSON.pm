86a2dd80d44565e0c16ea12b267e287c4a4324be
godsarmy <godsarmycy@gmail.com>
2014-04-14 22:43:56 +0800

    First Checkin of Template::JSON
    JSON template implementation for Perl
    http://json-template.googlecode.com/svn/trunk/doc/Introducing-JSON-Template.html

---
+++ Template/JSON.pm
@@ -0,0 +1,37 @@
+package Template::JSON;
+
+use strict;
+use warnings;
+
+sub new {
+    my $class = shift;
+    my ($template_str) = @_;
+    my $self = {
+        group => undef,
+    };
+    return bless $self, $class
+}
+
+sub _from_section {
+    my $self = shift;
+}
+
+sub _statements {
+    my $self = shift;
+
+}
+
+######################################
+# None class method
+######################################
+sub expand {
+    my ($template_str, $data, $args) = @_;
+    my $template = Template::JSON->new($template_str, $args);
+    return $template->expand($data);
+}
+
+sub _compile_template {
+
+}
+
+1;
